40ae63383dad0dd62594719a43feb564546752c4
Nikita <ntomilin@ukr.net>
2021-09-27 23:01:45 +0300

    added env logging

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const github = require('@actions/github');
 try {
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(payload);
+    console.log(process.env)
 } catch (error) {
     core.setFailed(error.message);
 }
